069a41c98e29f3039440a4659e1efe6c91611b9c
carter <weinbergcarter@gmail.com>
2020-02-08 15:41:00 -0500

    Added and tested json decoding

--- elections/src/Main.elm
+++ elections/src/Main.elm
@@ -2,10 +2,9 @@ module Main exposing (..)
 
 import Browser
 import Html exposing (..)
-import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Http
-import Json.Decode exposing (Decoder, field, string)
+import Json.Decode as Decode exposing (Decoder, field, string, int, map4, list)
 
 
 -- MAIN
@@ -20,11 +19,13 @@ main =
     }
 
 -- MODEL
-
+-- Also see election data down below.
 type Model
   = Failure
   | Loading
-  | Success String
+  | Success (List ElectionData)
+
+
 
 init : () -> (Model, Cmd Msg)
 init _ =
@@ -34,18 +35,19 @@ init _ =
 
 
 type Msg = 
-    GotElectionInfo (Result Http.Error String)
+    GotElectionInfo (Result Http.Error (List ElectionData))
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-    GotElectionInfo result ->
-      case result of
-        Ok url ->
-          (Success url, Cmd.none)
+    case msg of 
+        GotElectionInfo result -> 
+            case result of
+                Ok electionData ->
+                    (Success electionData, Cmd.none)
 
-        Err _ ->
-          (Failure, Cmd.none)
+                Err _ ->
+                    (Failure, Cmd.none)
 
 
 
@@ -65,23 +67,31 @@ view : Model -> Html Msg
 view model =
   div []
     [ h2 [] [ text "Elections Coming Soon" ]
-    , viewGif model
+    , viewElections model
     ]
 
 
-viewGif : Model -> Html Msg
-viewGif model =
+viewElections : Model -> Html Msg
+viewElections model =
   case model of
     Failure ->
       div []
-        [ text "I could not load the election info for some reason."
-        ]
+        [ text "I could not load the election info for some reason." ]
 
     Loading ->
       text "Loading..."
 
-    Success url ->
-      text "Add New Election Information here"
+    Success electionInfo ->
+            ul [] (List.map viewElection electionInfo)
+            
+
+viewElection : ElectionData -> Html Msg
+viewElection election = 
+    div [] 
+        [ div [] [text ("The election is called " ++ election.name)]
+        , div [] [text ("The election day is " ++ election.electionDay)]
+        ]
+      
 
 
 
@@ -92,10 +102,28 @@ getElectionInfo : Cmd Msg
 getElectionInfo =
   Http.get
     { url = "https://www.googleapis.com/civicinfo/v2/elections?key=AIzaSyDntZTUcLkFXbB_aYmeGCf7U2CQkMYQ9gM"
-    , expect = Http.expectJson GotGif gifDecoder -- replace gotgif with some kind of structure or something. Create a decoder method properly
+    , expect = Http.expectJson GotElectionInfo electionDecoder -- replace  with some kind of structure or something. Create a decoder method properly
     }
 
 
-gifDecoder : Decoder String
-gifDecoder =
-  
+type alias ElectionData = 
+    { id : String
+    , name : String
+    , electionDay : String
+    , ocdDivisionId : String
+    }
+
+
+-- get the elections field
+electionDecoder : Decoder (List ElectionData)
+electionDecoder =
+    field "elections" 
+        (list electionDecoderHelper)
+
+electionDecoderHelper : Decoder ElectionData
+electionDecoderHelper = 
+    (map4 ElectionData
+            (field "id" string)
+            (field "name" string)
+            (field "electionDay" string)
+            (field "ocdDivisionId" string))
